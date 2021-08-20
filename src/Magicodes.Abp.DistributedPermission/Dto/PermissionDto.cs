9ce2b30c941a34efb896a3c9842992cacda69274
wenqiang.li <codelove1314@live.cn>
2021-08-20 18:54:47 +0800

    暂存修改

---
+++ src/Magicodes.Abp.DistributedPermission/Dto/PermissionDto.cs
@@ -0,0 +1,11 @@
+﻿using Volo.Abp.Authorization.Permissions;
+using Volo.Abp.Localization;
+using Volo.Abp.MultiTenancy;
+
+namespace Magicodes.Abp.DistributedPermission.Dto
+{
+    public class PermissionDto : PermissionDefinition
+    {
+        public PermissionDto() { }
+    }
+}
---
+++ src/Magicodes.Abp.DistributedPermission/Dto/PermissionGroupDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Authorization.Permissions;
+using Volo.Abp.Localization;
+using Volo.Abp.MultiTenancy;
+
+namespace Magicodes.Abp.DistributedPermission.Dto
+{
+    public class PermissionGroupDto : PermissionGroupDefinition
+    {
+        public PermissionGroupDto()
+        {
+
+        }
+
+        public new List<PermissionDto> Permissions { get; set; }
+    }
+}
